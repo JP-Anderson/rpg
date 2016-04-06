27cb7d98b053dd915908114581a7c862814a45d6
Jp <dahui58@hotmail.com>
2016-04-06 16:10:45 +0100

    Refactored battle loop to lose a level of indentation

--- src/combat/battle.py
+++ src/combat/battle.py
@@ -23,88 +23,87 @@ class Battle:
 		self.battleLoop()
 	
 	def battleLoop(self):
-		while(True):
-			roundCounter = 0
-			gameIsRunning = self.checkBothTeamsAlive()
-			if gameIsRunning:
-				print()
-				print()
-				roundCounter = roundCounter + 1
-				print("Round " + str(roundCounter))
-				time.sleep(1)
-				for fighter in self.fighters:
-					if fighter.checkIfAlive():
-						choice = 0
-						while choice != 2:
-							fightersToAttack = []
-							if fighter.isPlayable:
-								for potentialTarget in self.enemies:
-									if potentialTarget.status != Status.DEAD: fightersToAttack.append(potentialTarget)
-							else:
-								for potentialTarget in self.friendlies:
-									if potentialTarget.status != Status.DEAD: fightersToAttack.append(potentialTarget)
-							fighterNames = []
-							for fighter2 in fightersToAttack:
-									fighterNames.append(fighter2.name)
-							if len(fightersToAttack) > 0:
-								#Gooey.printTeamStats(self.friendlies, self.enemies)
-								choices = ["Attack","Ability","End Turn","Check stats"]
-								Gooey.printLine("It's " + fighter.name + "'s turn")
-								choice = Gooey.getUserInputWithList(fighter.name + " has " + str(fighter.ap) + " AP remaining.", choices)
-							
-								if choice == 0: # ATTACK
-									choice = Gooey.getUserInputWithList("Who do you want to attack, " + fighter.name + "?", fighterNames)
-									if fighter.weapon == None:
-										Gooey.printLine("No weapon equipped")										
+		roundCounter = 0
+		gameIsRunning = self.checkBothTeamsAlive()
+		while(gameIsRunning):
+			print()
+			print()
+			roundCounter = roundCounter + 1
+			print("Round " + str(roundCounter))
+			time.sleep(1)
+			for fighter in self.fighters:
+				if fighter.checkIfAlive():
+					choice = 0
+					while choice != 2:
+						fightersToAttack = []
+						if fighter.isPlayable:
+							for potentialTarget in self.enemies:
+								if potentialTarget.status != Status.DEAD: fightersToAttack.append(potentialTarget)
+						else:
+							for potentialTarget in self.friendlies:
+								if potentialTarget.status != Status.DEAD: fightersToAttack.append(potentialTarget)
+						fighterNames = []
+						for fighter2 in fightersToAttack:
+								fighterNames.append(fighter2.name)
+						if len(fightersToAttack) > 0:
+							#Gooey.printTeamStats(self.friendlies, self.enemies)
+							choices = ["Attack","Ability","End Turn","Check stats"]
+							Gooey.printLine("It's " + fighter.name + "'s turn")
+							choice = Gooey.getUserInputWithList(fighter.name + " has " + str(fighter.ap) + " AP remaining.", choices)
+						
+							if choice == 0: # ATTACK
+								choice = Gooey.getUserInputWithList("Who do you want to attack, " + fighter.name + "?", fighterNames)
+								if fighter.weapon == None:
+									Gooey.printLine("No weapon equipped")										
+								else:
+									weaponAPCost = int(fighter.weapon.values["ShotAP"])
+									if fighter.ap >= weaponAPCost:
+										fighterToAttack = fightersToAttack[choice]
+										fighterToAttack.defend(fighter.attack())
 									else:
-										weaponAPCost = int(fighter.weapon.values["ShotAP"])
-										if fighter.ap >= weaponAPCost:
-											fighterToAttack = fightersToAttack[choice]
-											fighterToAttack.defend(fighter.attack())
-										else:
-											Gooey.printLine("Not enough AP. " + fighter.weapon.values["Name"] + " requires " + str(weaponAPCost) + " AP to use.")
-											time.sleep(1)
-											Gooey.printLine("")
-								elif choice == 1: # ABILITY
-									abilityNames = fighter.abilityList.returnNameStrings()
-									abilityCosts = fighter.abilityList.returnAbilityCosts()
-									if len(abilityNames)>0:
-										abilityChoice = Gooey.getUserInputWithList("What ability do you want to use?",abilityNames)
-										abilityName = abilityNames[abilityChoice]
-										abilityCost = abilityCosts[abilityChoice]
-										
-										if fighter.ap >= abilityCosts[abilityChoice]:
-											fighterNames = []
-											targetableFighters = []
-											for abilityTarget in self.fighters:
-												if abilityTarget.status != Status.DEAD: 
-													fighterNames.append(abilityTarget.name)
-													targetableFighters.append(abilityTarget)
-											#print(targetableFighters)
-											#print(self.fighters)
-											targetChoice = Gooey.getUserInputWithList("Who would you like to target?", fighterNames)
-											fighter.useAbility(abilityChoice, targetableFighters[targetChoice])
-										else: 
-											Gooey.printLine("Not enough AP. " + abilityName + " costs " + str(abilityCost) + " AP to use.") 
-									else: Gooey.printLine("No abilities")
-								elif choice == 2: # SKIP
-									pass
-								elif choice == 3: 
-									Gooey.printLine("Printing stats")
-									Gooey.printTeamStats(self.friendlies, self.enemies)
-									time.sleep(1)
-									Gooey.printLine("")
-							else:
-								break
-							time.sleep(1)
-						fighter.ap = fighter.ap+fighter.startAP
-						if fighter.ap > fighter.maxAP: fighter.ap = fighter.maxAP
-					else:
-						pass
-				else: print("...")
-			else:
-				print("Game over!")
-				break
+										Gooey.printLine("Not enough AP. " + fighter.weapon.values["Name"] + " requires " + str(weaponAPCost) + " AP to use.")
+										time.sleep(1)
+										Gooey.printLine("")
+							elif choice == 1: # ABILITY
+								abilityNames = fighter.abilityList.returnNameStrings()
+								abilityCosts = fighter.abilityList.returnAbilityCosts()
+								if len(abilityNames)>0:
+									abilityChoice = Gooey.getUserInputWithList("What ability do you want to use?",abilityNames)
+									abilityName = abilityNames[abilityChoice]
+									abilityCost = abilityCosts[abilityChoice]
+									
+									if fighter.ap >= abilityCosts[abilityChoice]:
+										fighterNames = []
+										targetableFighters = []
+										for abilityTarget in self.fighters:
+											if abilityTarget.status != Status.DEAD: 
+												fighterNames.append(abilityTarget.name)
+												targetableFighters.append(abilityTarget)
+										#print(targetableFighters)
+										#print(self.fighters)
+										targetChoice = Gooey.getUserInputWithList("Who would you like to target?", fighterNames)
+										fighter.useAbility(abilityChoice, targetableFighters[targetChoice])
+									else: 
+										Gooey.printLine("Not enough AP. " + abilityName + " costs " + str(abilityCost) + " AP to use.") 
+								else: Gooey.printLine("No abilities")
+							elif choice == 2: # SKIP
+								pass
+							elif choice == 3: 
+								Gooey.printLine("")
+								Gooey.printLine("Printing stats")
+								Gooey.printTeamStats(self.friendlies, self.enemies)
+								time.sleep(1)
+								Gooey.printLine("")
+						else:
+							break
+						time.sleep(1)
+					fighter.ap = fighter.ap+fighter.startAP
+					if fighter.ap > fighter.maxAP: fighter.ap = fighter.maxAP
+				else:
+					pass
+			else: print("...")
+			
+			gameIsRunning = self.checkBothTeamsAlive()	 
 	
 	def determineTurnOrder(self):
 		charCount = len(self.fighters)
