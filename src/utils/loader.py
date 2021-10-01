ee2649ce146b3b4ed52ffbc4739cdb8b3a13c38a
dahui58 <dahui58@hotmail.com>
2021-10-01 16:04:10 +0100

    add armour loader and first test case

--- src/utils/loader.py
+++ src/utils/loader.py
@@ -1,7 +1,7 @@
 # loader
 from utils.csvreader import CsvReader
 from items.weapon import  Weapon
-
+from items.armour import Armour
 
 class Weapons:
 	
@@ -17,3 +17,18 @@ class Weapons:
 	
 	def list(self):
 		return self.weapons
+
+class Armours:
+
+    def __init__(self):
+        armour_csv = CsvReader.read("..\\data\\armour.csv")
+        keys = armour_csv[0]
+        numberOfObjects = len(armour_csv)
+        self.armour = []
+        print("  Loading Armour")
+        for i in range (1, numberOfObjects):
+            self.armour.append(Armour(keys, armour_csv[i]))
+            print("    - " + str(armour_csv[i][1]))
+
+    def list(self):
+        return self.armour
